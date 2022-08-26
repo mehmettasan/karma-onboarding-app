663ab7fe94180cfb1fc86c80150ae1deae5bfc74
mehmettasan <83862655+mehmettasan@users.noreply.github.com>
2022-08-26 21:58:54 +0300

    added gradient background

--- src/pages/WelcomePage/WelcomePage.js
+++ src/pages/WelcomePage/WelcomePage.js
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import LinearGradient from 'react-native-linear-gradient'
+import styles from "./WelcomePage.style"
 
 const WelcomePage = () => {
   return (
-    <View>
+    <LinearGradient colors={["#EAE0FF","#FFFFFF"]} style={styles.container}>
       <Text>WelcomePage</Text>
-    </View>
+    </LinearGradient>
   )
 }
 
